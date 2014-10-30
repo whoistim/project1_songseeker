50c6347e55e7b409c90a6e234c5de197eddaa75e
whoistim <whoistim@sbcglobal.net>
2014-10-30 13:10:57 -0700

    ready for heroku

--- app.js
+++ app.js
@@ -156,7 +156,4 @@ app.get('*', function(req,res){
   res.render('404');
 });
 
-
-app.listen(3000, function(){
-  console.log("get this party started on port 3000");
-});
+app.listen(process.env.PORT || 3000);
